29cd690ccadb37bdc18cfc902f8deb9038a0a777
Yuichiro MASUI <masui@masuidrive.jp>
2018-03-18 00:38:16 +0900

    WIP

--- components/Timetable.js
+++ components/Timetable.js
@@ -45,18 +45,17 @@ export default class TimeTable extends React.Component {
     const resources = applySettingsToResources(this.props.resources, this.props.settings)
     return (
       <div ref="wrapper" style={{width: this.props.width, height: this.props.height, overflow: "auto", margin:0,padding:0}}>
-          <div style={{width: labelWidth, height: resourceHeight, zIndex:4, left:0, top:0, position: 'absolute',display: 'block', backgroundColor: 'blue'}}>
-            123
+          <div style={{width: labelWidth, height: resourceHeight, zIndex:4, left:0, top:0, position: 'absolute',display: 'block', backgroundColor: '#689090'}}>
           </div>
         <div style={{width: labelWidth + hourWidth * 24, height: resourceHeight * resources.length + labelHeight, position: 'relative', margin:0,padding:0}}>
           <div style={{width: hourWidth * 24, height: resourceHeight, zIndex:1,left:labelWidth,top:0,position: 'sticky'}}>
             { Array(24).fill(0).map((x, i) => (
-              <div style={{backgroundColor: 'red', width:hourWidth, height: resourceHeight, left:hourWidth*i,top:0,position:'absolute'}}>{i}:00</div>
+              <div style={{backgroundColor: '#C8D4CB', width:hourWidth, height: resourceHeight, left:hourWidth*i,top:0,position:'absolute'}}>{i}:00</div>
             )) }
           </div>
           <div style={{width: labelWidth, height: resourceHeight * resources.length, left: 0, top:0,zIndex:2,position: 'sticky', backgroundColor: 'gray'}}>
             { resources.map((resource, resourceIndex) => (
-              <div style={{backgroundColor: 'red', width:labelWidth, height: resourceHeight, left:0,top:resourceIndex*resourceHeight,position:'absolute'}}>{resource.name}</div>
+              <div style={{backgroundColor: '#C8D4CB', width:labelWidth, height: resourceHeight, left:0,top:resourceIndex*resourceHeight,position:'absolute'}}>{resource.name}</div>
             )) }
           </div>
           { resources.map((resource, resourceIndex) => (
@@ -67,7 +66,7 @@ export default class TimeTable extends React.Component {
               const x = start * hourWidth / 60 + labelWidth
               const width = (end - start) * hourWidth / 60
               return(
-                <div style={{border: "1px solid blue", overflow: 'hidden', position: 'absolute', height: `${resourceHeight}px`, width: `${width}px`, top: `${y}px`, left: `${x}px`}}>
+                <div style={{backgroundColor: "#2796CB",color: "white", fontSize: 12, padding: 2, border: "1px solid white", borderRadius: 6, overflow: 'hidden', position: 'absolute', height: `${resourceHeight}px`, width: `${width}px`, top: `${y}px`, left: `${x}px`}}>
                   {event.title}
                 </div>
               )
