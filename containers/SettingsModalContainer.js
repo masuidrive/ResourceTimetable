f1d0bee507367136a25b751a4b6474321e30e184
Yuichiro MASUI <masui@masuidrive.jp>
2018-03-19 23:34:42 +0900

    WIP

---
+++ containers/SettingsModalContainer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+
+import { hideSettingsModal, resetResourceSettings } from '../store'
+import SettingsContainer from '../containers/SettingsContainer'
+
+export default connect(
+  (state) => ({ // mapStateToPropsContainer
+    shownSettingsModal: state.shownSettingsModal
+  }),
+  (dispatch) => ({ // mapDispatchToProps
+    resetResourceSettings: bindActionCreators(resetResourceSettings, dispatch),
+    hideSettingsModal: bindActionCreators(hideSettingsModal, dispatch),
+  })
+)(({shownSettingsModal, hideSettingsModal, resetResourceSettings}) => {
+  if(!shownSettingsModal) { return <div></div> }
+
+  return <div>
+    <Modal open={true} size="tiny" >
+      <Header icon="settings" content="Settings" />
+      <Modal.Content >
+        <SettingsContainer />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={() => resetResourceSettings()}>All reset</Button>
+        <Button primary onClick={() => hideSettingsModal()}>
+          <Icon name='checkmark' /> Close
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  </div>
+})
