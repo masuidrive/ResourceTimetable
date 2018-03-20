4ae7df5ad5bdc58aa33803c616e972252b27cf1d
Yuichiro MASUI <masui@masuidrive.jp>
2018-03-21 03:28:58 +0900

    WIP

--- components/Timetable.js
+++ components/Timetable.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { findDOMNode } from 'react-dom'
 import moment from 'moment-timezone'
+import { Button } from 'semantic-ui-react'
+
 
 const applySettingsToResources = (resources, settings) => {
   // sort by resource name
@@ -59,23 +61,24 @@ export default class TimeTable extends React.Component {
     const resources = applySettingsToResources(this.props.resources, this.props.settings)
     return (
       <div>
-        <div style={{width: labelWidth, height: resourceHeight, zIndex:4, left:"5vw", marginTop: '5vh', top:headerHeight, position: 'absolute', display: 'block', backgroundColor: '#ECECEB',borderRight: "1px solid #888", borderBottom: "1px solid #888"}}>&lt; 3/15 &gt;</div>
+        <div style={{width: labelWidth, height: resourceHeight, zIndex:4, left:"0vw", marginTop: '0vh', top:headerHeight, position: 'absolute', display: 'block', backgroundColor: '#ECECEB',borderRight: "1px solid #888", borderBottom: "1px solid #888"}}>&lt; 3/15 &gt;</div>
 
-      <div ref="wrapper" style={{position: 'absolute',top:"5vh",left:"5vw", width:'90vw', height: '90vh', overflow: "auto", padding:0 ,margin:0, borderTop: "34px solid transparent",WebkitOverflowScrolling: "touch"}}>
+      <div ref="wrapper" style={{position: 'absolute',top:"0vh",left:"0vw", width:'100vw', height: '100vh', overflow: "auto", padding:0 ,margin:0, borderTop: `${headerHeight}px solid transparent`,WebkitOverflowScrolling: "touch"}}>
         <div style={{width: labelWidth + hourWidth * 24, height: resourceHeight * resources.length + labelHeight, position: 'relative', margin:0,padding:0,backgroundColor: 'white'}}>
-          <div style={{width: hourWidth * 24, height: resourceHeight, zIndex:1,left:labelWidth,top:0,position: '-webkit-sticky',position: 'sticky'}}>
+          <div style={{width: hourWidth * 24, height: resourceHeight, zIndex:1,left:labelWidth,top:0}} className="sticky">
             { Array(24).fill(0).map((x, i) => (
               <div style={{backgroundColor: '#ECECEB', borderRight: "1px dashed #ddd", borderBottom: "1px solid #888", padding: "2px", width:hourWidth, height: resourceHeight, left:hourWidth*i,top:0,position:'absolute'}} key={`hour-${i}`}>{i}:00</div>
             )) }
           </div>
-          <div style={{width: labelWidth, height: resourceHeight * resources.length, left: 0, top:0,zIndex:2,position: '-webkit-sticky',  position: 'sticky'}}>
+          <div style={{width: labelWidth, height: resourceHeight * resources.length, left: 0, top:0,zIndex:2}} className="sticky">
             { resources.map((resource, resourceIndex) => (
-              <div style={{width:labelWidth, height: resourceHeight, left:0,top:resourceIndex*resourceHeight,position:'absolute',borderBottom:"1px solid #ddd",borderRight:"1px solid #888",backgroundColor:'#ECECEB'}} key={`label-${resource.calendarId}`}>{resource.name}</div>
+              <div style={{width:labelWidth, height: resourceHeight, left:0,top:resourceIndex*resourceHeight,position:'absolute',borderBottom:"1px solid #ddd",borderRight:"1px solid #888",padding: "1px 4px", backgroundColor:'#ECECEB'}} key={`label-${resource.calendarId}`}>{resource.name}</div>
             )) }
           </div>
+
           { Array(resources.length+1).fill(0).map((x, i) => (
             <div style={{borderBottom: '1px solid #ddd', width:hourWidth*24+labelWidth, height: 1, left:0,top:i*resourceHeight+resourceHeight-1,position:'absolute'}} key={`resourcec-line-${i}`}></div>
-          ))}
+          ))}          
           { Array(24).fill(0).map((x, i) => (
             <div style={{borderRight: '1px dashed #ddd', width:1, height: resourceHeight*(resources.length+1), left:hourWidth*(i+1)-1,top:0,position:'absolute'}} key={`hour-k-${i}`}></div>
           )) }
